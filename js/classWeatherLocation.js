e67544ba507bf833303293f8c92ecbd0bd371c6d
אודיה סעדון <osadoun@g.jct.ac.il>
2023-11-13 18:59:51 +0200

    add icons to cards

--- js/classWeatherLocation.js
+++ js/classWeatherLocation.js
@@ -6,8 +6,20 @@ class WeatherLocation {
     this.temp = _item.main.temp;
     this.feels_like = _item.main.feels_like;
     this.humidity = _item.main.humidity;
-    this.icon = _item.weather.icon;
     this.name = _item.name;
+  }
+
+  currentIcon() {
+   
+    if(Math.round(this.temp) < 20){
+        return `<i class="fa fa-bolt" aria-hidden="true" style="color:rgb(42, 6, 141)"></i>`;
+    }
+    else if(Math.round(this.temp) < 30){
+        return `<i class="fa fa-cloud" aria-hidden="true" style="color:lightblue"></i>`;
+    }
+    else{
+        return `<i class="fa fa-sun-o" aria-hidden="true" style="color:gold"></i>`;
+    }
   }
 
   render() {
@@ -16,25 +28,34 @@ class WeatherLocation {
     let loc = document.createElement("div");
     loc.innerHTML = `
         <div class="card m-3">
-        <img src="../images/${en_name_lower}.jpg"  class="card-img-top image_loc" alt="${this.en_name}">
-        <div class="card-body">
-          <h5 class="card-title">${this.name}</h5>
-          <p class="card-text">${this.description}</p>
-          <div class="temps d-flex justify-content-between">
-          <div class=""d-dlex flex-column>
-          <p>טמפ'</p>
-          <p class="text-center ltr">${Math.round(this.temp)}&deg;C</p>
-          </div>
-          <div class=""d-dlex flex-column>
-          <p>טמפ' מורגשת</p>
-            <p class="text-center ltr"> ${Math.round(this.feels_like)}&deg;C</p>
-          </div>
-          <div class=""d-dlex flex-column>
-          <p>לחות</p>
-          <p class="text-center">${this.humidity}%</p>
-          </div>
-          </div>
-        </div>
+        <img src="../images/${en_name_lower}.jpg"  class="card-img-top image_loc" alt="${
+      this.en_name
+    }">
+        <div class="card-body d-flex flex-column">
+            <div class="d-flex my-3 justify-content-between">
+                <div>
+                    <h2 class="card-title">${this.name}</h2>
+                    <p class="card-text">${this.description}</p>
+                </div>
+                <div class="icon_div">
+                ${this.currentIcon()}
+                </div>
+            </div>
+            <div class="temps d-flex justify-content-between">
+                <div class=""d-dlex flex-column>
+                    <p>טמפ'</p>
+                    <p class="text-center ltr">${Math.round(this.temp)}&deg;C</p>
+                </div>
+                <div class=""d-dlex flex-column>
+                    <p>טמפ' מורגשת</p>
+                    <p class="text-center ltr"> ${Math.round(this.feels_like)}&deg;C</p>
+                </div>
+                <div class=""d-dlex flex-column>
+                    <p>לחות</p>
+                    <p class="text-center">${this.humidity}%</p>
+                </div>
+            </div>
+      </div>
       </div>
         `;
     loc.className = "col-lg-3 col-md-6";
