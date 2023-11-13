ea1d157818f9d53d654cb32cd8ff0887d93713de
אודיה סעדון <osadoun@g.jct.ac.il>
2023-11-13 18:00:17 +0200

    use axios instead of fatch

--- js/appWeather.js
+++ js/appWeather.js
@@ -7,29 +7,15 @@ window.onload = async() => {
     for (const loc_name of locations_ar) {
         await getLocFromName(loc_name);
     }
-    // for (const loc of locations_obj_ar) {
-    //     await getFullInfo(loc);
-    // }
 };
-
-// const getFullInfo = async(_data) => {
-//     console.log("data", _data);
-//   let url = `https://api.openweathermap.org/data/2.5/weather?lat=${_data.lat}&&lon=${_data.lon}&appid=${API_KEY}&lang=${LANG}`;
-
-//   let resp = await fetch(url)
-//   let data = await resp.json()
-//   console.log("11data" , data);
-//     let loc = new WeatherLocation(".row", data, _data);
-//     loc.render();
-// };
 
 
 const getLocFromName = async (_loc_name) => {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${_loc_name}&units=metric&lang=he&appid=${API_KEY}`;
 
-  let resp = await fetch(url)
-  let data = await resp.json()
-  console.log("11data" , data);
-    let loc = new WeatherLocation(".row", data, _loc_name);
-    loc.render();
+    axios.get(url)
+    .then((resp) =>{
+        let loc = new WeatherLocation(".row", resp.data, _loc_name);
+        loc.render();
+    })
 };
